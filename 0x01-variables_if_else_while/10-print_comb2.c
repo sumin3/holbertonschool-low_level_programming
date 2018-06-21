d77ab2ba1f521feca1a777ef6a4ffa8e7d1c3710
sumin3 <suminyu@yahoo.com>
2018-06-21 22:03:08 +0000

    add file for task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print all the single digit numbers separated by , and  a space
+ * Return: 0
+ */
+int main(void)
+{
+	int tens_digit = 0;
+	int units_digit = 0;
+
+	while (tens_digit <= 9 && units_digit <= 9)
+	{
+		putchar('0' + tens_digit);
+		putchar('0' + units_digit);
+		if (tens_digit != 9 || units_digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (units_digit == 9)
+		{
+			tens_digit++;
+			units_digit = 0;
+		}
+		units_digit++;
+	}
+	putchar('\n');
+	return (0);
+}
