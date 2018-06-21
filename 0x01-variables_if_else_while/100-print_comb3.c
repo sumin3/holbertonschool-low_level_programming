a8289ba111415021ef3b07a8f4de105dc77d53ad
sumin3 <suminyu@yahoo.com>
2018-06-21 22:34:07 +0000

    add file for 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print all the single digit numbers separated by , and  a space
+ * Return: 0
+ */
+int main(void)
+{
+	int tens_digit = 0;
+	int units_digit = 1;
+
+	while (tens_digit <= 9 && units_digit <= 9)
+	{
+		if (units_digit > tens_digit)
+		{
+			putchar('0' + tens_digit);
+			putchar('0' + units_digit);
+			if (tens_digit != 8 || units_digit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		if (units_digit == 9)
+		{
+			tens_digit++;
+			units_digit = 0;
+		}
+		units_digit++;
+	}
+	putchar('\n');
+	return (0);
+}
