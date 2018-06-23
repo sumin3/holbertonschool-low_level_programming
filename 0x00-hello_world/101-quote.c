67a5dcb29161c2e8f4431c26caa8204948b9eea3
sumin3 <suminyu@yahoo.com>
2018-06-23 00:19:18 +0000

    add 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-        return (0);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 59);
+	return (0);
 }
