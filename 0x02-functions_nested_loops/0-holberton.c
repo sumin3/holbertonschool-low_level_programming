97d80dfca3ccc6a66b88a157f82958b2a8f9d42a
sumin3 <suminyu@yahoo.com>
2018-06-25 23:27:36 +0000

    use betty format

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
+/**
+ * main - print holberton
+ * Return: 0
+ */
 int main(void)
 {
 	int i;
 	char message[10] = "Holberton\n";
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(message[i]);
