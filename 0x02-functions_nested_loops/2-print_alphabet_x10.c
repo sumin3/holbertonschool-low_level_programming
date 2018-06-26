bbb2ba2b9b51dab19ddb9b7796a908183c946c6b
sumin3 <suminyu@yahoo.com>
2018-06-26 17:22:41 +0000

    remove space in front of ++

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 		_putchar('\n');
 	}
