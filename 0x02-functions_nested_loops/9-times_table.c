f565a624039ae0fa4f9002c8bb5170e4a21c78c1
sumin3 <suminyu@yahoo.com>
2018-06-26 18:57:38 +0000

    add last digit and frist digit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,31 @@ void times_table(void)
 {
 	int num1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int num2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int i = 0;
+	int i, j;
+	int result;
+	int first_digit;
+	int last_digit;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(num1[i] * num2[j]);
-			if (j != 9)
+			result = num1[i] * num2[j];
+			first_digit = result / 10;
+			last_digit = result % 10;
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+			}
+			if (result < 10)
+				_putchar('0' + result);
+			else
+			{
+				_putchar('0' + first_digit);
+				_putchar('0' + last_digit);
 			}
 		}
 		_putchar('\n');
