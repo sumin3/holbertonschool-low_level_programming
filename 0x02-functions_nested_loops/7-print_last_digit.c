69dd419812c6debcd57749176567fa1aa9b1d45a
sumin3 <suminyu@yahoo.com>
2018-06-26 18:02:44 +0000

    switch last_digit and num

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,10 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	if (num < 0)
-		num = -num;
 	last_digit = (num % 10);
+	if (last_digit < 0)
+		last_digit = -last_digit;
 	_putchar('0' + last_digit);
+
 	return (last_digit);
 }
