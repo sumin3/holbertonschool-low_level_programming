ebd79be8c44e128f39e8762608ac94dce9204eb4
sumin3 <suminyu@yahoo.com>
2018-06-26 23:00:31 +0000

    use long int for all data type

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,10 +8,10 @@ int main(void)
 	long int a = 1;
 	long int b = 2;
 	long int f;
-	long int i;
+        long int i;
 	long int even_sum = 2;
 
-	for (i = 0; i <= 4000000 ; i++)
+	for (i = 0; i <= 4000000 - 3 ; i++)
 	{
 		f = a + b;
 		a = b;
@@ -19,6 +19,6 @@ int main(void)
 		if ((f % 2) == 0)
 			even_sum = even_sum + f;
 	}
-	printf("%ld\n", even_sum);
+	printf("%lu\n", even_sum);
 	return (0);
 }
