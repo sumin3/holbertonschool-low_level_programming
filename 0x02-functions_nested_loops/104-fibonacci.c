cdff2e2c89d72971b7bc2fa8656b6ab715ea76dc
sumin3 <suminyu@yahoo.com>
2018-06-27 02:24:25 +0000

    add new file for task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,49 @@ int main(void)
 	long int b = 2;
 	long int f;
 	int i;
+	long int a1, a2, b1, b2, sum1, sum2, overflow1, overflow2, p1, p2;
 
 	printf("%lu, %lu, ", a, b);
-	for (i = 0; i <= 98 - 3; i++)
+	for (i = 0; i <= 98 - 4; i++)
 	{
-		f = a + b;
-		a = b;
-		b = f;
-		if (i == (98 - 3))
-			printf("%lu", f);
-		else
+		if (i < 89)
+		{
+			f = a + b;
+			a = b;
+			b = f;
 			printf("%lu, ", f);
+		}
+		else if (i == 89)
+		{
+			a1 = a / 10000000000;
+			a2 = a % 10000000000;
+			b1 = b / 10000000000;
+			b2 = b % 10000000000;
+			sum2 = a2 + b2;
+			p2 = sum2 % 10000000000;
+			overflow2 = sum2 / 10000000000;
+			sum1 = a1 + b1 + overflow2;
+			p1 = sum1 % 10000000000;
+			overflow1 = sum1 / 10000000000;
+			printf("%lu%lu, ", p1, p2);
+		}
+		else if (i > 89)
+		{
+			a1 = b1;
+			a2 = b2;
+			b1 = sum1;
+			b2 = p2;
+			sum2 = a2 + b2;
+			p2 = sum2 % 10000000000;
+			overflow2 = sum2 / 10000000000;
+			sum1 = a1 + b1 + overflow2;
+			p1 = sum1 % 10000000000;
+			overflow1 = sum1 / 10000000000;
+			if (i == 94)
+				printf("%lu%lu%lu", overflow1, p1, p2);
+			else
+				printf("%lu%lu, ", p1, p2);
+		}
 	}
 	printf("\n");
 	return (0);
