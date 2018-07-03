53626d877e723816b5492911e0e79f8294b35492
sumin3 <suminyu@yahoo.com>
2018-07-03 17:26:51 +0000

    betty check

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d",a[i]);
-		if (i != (n-1))
+		printf("%d", a[i]);
+		if (i != (n - 1))
 			printf(", ");
 		else
 			printf("\n");
