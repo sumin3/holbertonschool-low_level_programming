e9ec8f2a4d4e2e7d417cb7c5fb56c911b888a027
sumin3 <suminyu@yahoo.com>
2018-07-03 19:13:38 +0000

    add a char h

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * rev_string - Write a function that reverses a string.
  * @s: the string that need to reverse
  */
 void rev_string(char *s)
 {
-	int i = 0, length = 0, index = 0;
+	int length = 0, index = 0;
+	int i=0;
+	char h[10];
 
 	while (s[length] != '\0')
 		length++;
-	for (index = length; index >= 0; index--)
+	for (index = length - 1; index >= 0; index--)
 	{
-		s[i] = s[index - 1];
+		h[i] = s[index];
+		s[i] = h[i];
 		i++;
 	}
 }
