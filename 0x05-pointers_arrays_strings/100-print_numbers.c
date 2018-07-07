f006e4e638b5324cdcb6d7a78700e7b94e7e6565
sumin3 <suminyu@yahoo.com>
2018-07-07 23:50:08 +0000

    add new file

---
+++ 0x05-pointers_arrays_strings/100-print_numbers.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - prints an integer
+ * @n: the number that needed to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		if (n / 10)
+		{
+			_putchar('-');
+			print_number((n / 10) * -1);
+		}
+		else
+			_putchar((n % 10) * (-1) + '0');
+
+	}
+	if (n >= 0)
+	{
+		if (n / 10)
+		{
+			print_number(n / 10);
+			_putchar((n % 10) + '0');
+		}
+		else
+			_putchar((n % 10) + '0');
+	}
+}
