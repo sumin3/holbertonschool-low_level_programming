100b972092b98f296f7212f5f86973f1ba3855ff
sumin3 <suminyu@yahoo.com>
2018-07-08 01:27:06 +0000

    add a if statement for index 0

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -18,6 +18,8 @@ char *cap_string(char *s)
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] = s[i + 1] - 32;
 		}
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] = s[0] - 32;
 	}
 	return (s);
 }
