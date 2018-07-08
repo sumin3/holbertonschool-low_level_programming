4035fa1f9c81b685eba23f4bfa5040d9d1b3a756
sumin3 <suminyu@yahoo.com>
2018-07-08 00:09:55 +0000

    rewrite code

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -6,19 +6,26 @@
  */
 void print_number(int n)
 {
-	
 	if (n < 0)
-		_putchar('-');
+	{
+		if (n / 10)
+		{
+			_putchar('-');
+			print_number((n / 10) * -1);
+			_putchar((n % 10) * (-1) + '0');
+		}
+		else
+			_putchar((n % 10) * (-1) + '0');
+
+	}
 	if (n >= 0)
 	{
 		if (n / 10)
 		{
 			print_number(n / 10);
-			_putchar(n % 10 + '0');
+			_putchar((n % 10) + '0');
 		}
 		else
-			_putchar(n % 10 + '0');
+			_putchar((n % 10) + '0');
 	}
-	
-	
 }
