b556f12414bbf779aa26c2c3c21512618cfc41c7
sumin3 <suminyu@yahoo.com>
2018-07-08 02:45:20 +0000

    new file

--- 0x05-pointers_arrays_strings/101-magic.c
+++ 0x05-pointers_arrays_strings/101-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - change a[2] = 98
+ * Return: 0;
+ */
 int main(void)
 {
 	int n;
@@ -8,17 +11,15 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	*p = ;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	;
-	/* ...so that this prints 98\n */
+	*(&n + 5) = 98;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
