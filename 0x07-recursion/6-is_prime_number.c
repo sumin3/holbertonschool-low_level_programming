12137b62864defe977009237356d0a05ba33fdd5
sumin3 <suminyu@yahoo.com>
2018-07-11 23:10:38 +0000

    betty check

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -6,9 +6,14 @@
  */
 int is_prime_number(int n)
 {
-	return(helper(n, 2));
+	return (helper(n, 2));
 }
-
+/**
+ * helper - help function to find prime number
+ * @n: the number need to check
+ * @f: the factor number to help determine is a prime number or not
+ * Return: return 0 if is prime, otherwise, return 1
+ */
 int helper(int n, int f)
 {
 	if (n < 2)
