3264c49bb59f8817cdc5bc266b4e0b4ea04f81aa
sumin3 <suminyu@yahoo.com>
2018-07-11 18:22:27 +0000

    putchar new line instead of return

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -6,9 +6,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return;
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	if (*(s + 1) == '\0')
 		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
