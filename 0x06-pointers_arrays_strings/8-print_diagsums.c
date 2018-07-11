369ce0c3b3ed6598a60cce337cca38fc04bbe72c
sumin3 <suminyu@yahoo.com>
2018-07-11 04:12:05 +0000

    change the second for loop <= size to add one more num to sum

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j = 0, sum = 0, sum2 = 0, add = 0, add2 = 0;
+	int i, j, sum = 0, sum2 = 0, add = 0, add2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -16,9 +16,9 @@ void print_diagsums(int *a, int size)
 		sum = sum + *(a + add);
 	}
 	printf("%d, ", sum);
-	for (j = 0; j < size; j++)
+	for (j = 1; j <= size; j++)
 	{
-		add2 = j * (size - 1);
+		add2 = (size - 1) * j;
 		sum2 = sum2 + *(a + add2);
 	}
 	printf("%d\n", sum2);
