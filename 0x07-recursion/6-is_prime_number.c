9af1f35ed32ddc08310c94f3daab5fddb878da19
sumin3 <suminyu@yahoo.com>
2018-07-11 23:06:17 +0000

    add new file

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * is_prime_number - determine a input integer is a prime number or not
+ * @n: the number that need to determine
+ * Return: return 1 if is prime, return 0 is not
+ */
+int is_prime_number(int n)
+{
+	return(helper(n, 2));
+}
+
+int helper(int n, int f)
+{
+	if (n < 2)
+		return (0);
+	if (n == f)
+		return (1);
+	else if (n % f == 0)
+		return (0);
+	return (helper(n, (f + 1)));
+}
