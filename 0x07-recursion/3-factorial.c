70b8d50b1cd107e16de2d9a47ad9918046ca6c5e
sumin3 <suminyu@yahoo.com>
2018-07-11 20:25:05 +0000

    add new file

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number need to fine the factorial
+ * Return: return the factorial.
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n > 0)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
