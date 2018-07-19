a33d04fafa2cfa870e6b96c4c4481f70f46c538e
sumin3 <suminyu@yahoo.com>
2018-07-19 23:12:43 +0000

    fill the array with 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -8,12 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *cal;
+	char *cal;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	cal = malloc(size * nmemb);
 	if (cal == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		cal[i] = 0;
 	return (cal);
 }
