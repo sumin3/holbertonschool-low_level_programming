f1236869c290964020b975e047723ca167b26214
sumin3 <suminyu@yahoo.com>
2018-07-19 20:17:34 +0000

    put '\0' at the end of string

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -29,12 +29,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (concat == NULL)
 		return (NULL);
 	/* copy string */
-	for (i = 0; i < len1 + n + 1; i++)
+	for (i = 0; i < len1 + n; i++)
 	{
 		if (i < len1)
 			concat[i] = s1[i];
 		else
 			concat[i] = s2[i - len1];
 	}
+	concat[i] = '\0';
 	return (concat);
 }
