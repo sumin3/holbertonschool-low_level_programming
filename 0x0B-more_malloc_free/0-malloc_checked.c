de3ed0c62220725393c8405108074baf353b602e
sumin3 <suminyu@yahoo.com>
2018-07-19 18:08:21 +0000

    add new file

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of memory space
+ * Return: if malloc fails, the malloc_checked function should cause normal
+ * process termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *mal;
+	/* malloc space for mal */
+	mal = malloc(b);
+	/* if malloc space fail exit 98 */
+	if (mal == NULL)
+		exit(98);
+	return (mal);
+}
