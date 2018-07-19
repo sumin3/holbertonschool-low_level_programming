d9cdce32a278f1e6e0a4ece1c80a390d30fa82b2
sumin3 <suminyu@yahoo.com>
2018-07-19 23:54:06 +0000

    change <= to <

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 	tmp = min;
-	for (i = 0; i <= (max - min + 1); i++)
+	for (i = 0; i < (max - min + 1); i++)
 	{
 		array[i] = tmp;
 		tmp++;
