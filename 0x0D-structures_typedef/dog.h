b975c56a50f7da3c060d82d9ef20d51162e1404a
sumin3 <suminyu@yahoo.com>
2018-07-23 21:16:43 +0000

    use typedef

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: second member for dog's age
  * @owner: third member for dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
