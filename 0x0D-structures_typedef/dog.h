ab5b3b3e1ffbae0b3d8530fbb5a1b567870a238e
sumin3 <suminyu@yahoo.com>
2018-07-23 20:10:20 +0000

    add new file

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct for dog information
+ * @name: first member for dog's name
+ * @age: second member for dog's age
+ * @owner: third member for dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
