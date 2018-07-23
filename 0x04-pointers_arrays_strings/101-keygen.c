f122d2051b7ef5adf99603daf0fa20c52df36187
sumin3 <suminyu@yahoo.com>
2018-07-23 04:29:28 +0000

    fix logic

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	int rand_num, sum;
+	int rand_num, sum = 0;
 
 	rand_num = rand() % 128;
-	while (sum < 2772)
+	while (sum < 2772 - 128)
 	{
+		_putchar(rand_num);
 		sum = sum + rand_num;
-		if (sum <= 2772)
-			putchar(rand_num);
-	        else
-			break;
 	}
+	rand_num = 2772 - sum;
+	_putchar(rand_num);
 	return (0);
 }
