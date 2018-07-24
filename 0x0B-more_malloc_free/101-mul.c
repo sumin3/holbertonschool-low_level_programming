0b8043e1fa256c4bb76f2db0ab3524f6c5b86caa
sumin3 <suminyu@yahoo.com>
2018-07-24 21:52:48 +0000

    add zero to buffer

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -1,9 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
-void print_number(int n);
 int _isdigit(int c);
-void print_error();
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_zero(char *sum, int num);
 /**
@@ -14,24 +12,22 @@ char *add_zero(char *sum, int num);
  */
 int main(int argc, char *argv[])
 {
-	int i, j, k, h, p, num2 = 0, len1 = 0, len2 = 0;
+	int i, j, k, h, num2 = 0, len1 = 0, len2 = 0;
 	char *mul;
 	char *num1;
 
 	if (argc - 1 != 2)
 	{
-		print_error();
+		printf("Error\n");
 		exit(98);
 	}
-	printf("c: %d\n", argc);
-	printf("v: %s\n", argv[1]);
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!_isdigit(argv[i][j]))
 			{
-				print_error();
+				printf("Error\n");
 				exit(98);
 			}
 			if (i == 1)
@@ -40,7 +36,6 @@ int main(int argc, char *argv[])
 				len2++;
 		}
 	}
-	printf("len1 %d, len2 %d\n", len1, len2);
 	mul = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (mul == NULL)
 		exit(98);
@@ -51,23 +46,24 @@ int main(int argc, char *argv[])
 	for (k = 0; argv[2][k] != '\0'; k++)
 	{
 		num2 = argv[2][k] - '0';
-		printf("current num: %d\n",num2);
-		for (p = 0; argv[1][p] != '\0'; p++)
-			num1[p] = argv[1][p];
-		num1[p] = '\0';
-		for (h = 0; h < num2 - 1; h++)
+		num1 = infinite_add("0", "0", num1, len1 + len2 + 1);
+		for (h = 0; h < num2; h++)
 		{
-			num1 = infinite_add(num1,argv[1], num1, len1 + len2 + 1);
+			num1 = infinite_add(num1, argv[1], num1, len1 +
+					    len2 + 1);
 		}
-		printf("sum: %s\n", num1);
-		num1 = add_zero(num1, len2 - k -1);
-		printf("zero: %s\n", num1);
+		num1 = add_zero(num1, len2 - k - 1);
 		mul = infinite_add(mul, num1, mul, len1 + len2 + 1);
-		printf("product: %s\n", mul);
-
 	}
+	printf("%s\n", mul);
 	return (0);
 }
+/**
+ * add_zero - add zero at the end of string
+ * @num: number of zero need to add
+ * @sum: the string that need to add zero
+ * Return: return a pointer of string that contain zero
+ */
 char *add_zero(char *sum, int num)
 {
 	int len = 0, i = 0;
@@ -81,49 +77,7 @@ char *add_zero(char *sum, int num)
 	sum[i] = '\0';
 	return (sum);
 }
-/**
- * print_error - print error message
- */
-void print_error()
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-}
-/**
- * print_number - prints an integer
- * @n: the number that needed to print
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		if (n / 10)
-		{
-			print_number(n / 10);
-			_putchar((n % 10) * (-1) + '0');
-		}
-		else
-		{
-			_putchar('-');
-			_putchar((n % 10) * (-1) + '0');
-		}
 
-	}
-	if (n >= 0)
-	{
-		if (n / 10)
-		{
-			print_number(n / 10);
-			_putchar((n % 10) + '0');
-		}
-		else
-			_putchar((n % 10) + '0');
-	}
-}
 /**
  * _isdigit - check for a digit (0 to 9)
  * @c: the number to be checked
@@ -144,59 +98,59 @@ int _isdigit(int c)
  * @size_r: the buffer size
  * Return: return the sum (r)
  */
-	char *infinite_add(char *n1, char *n2, char *r, int size_r)
-	{
-		int len, len1 = 0, len2 = 0, i, j;
-		int overflow = 0, a1, a2, sum = 0, sum1 = 0;
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len, len1 = 0, len2 = 0, i, j;
+	int overflow = 0, a1, a2, sum = 0, sum1 = 0;
 
-		while (n1[len1++] != '\0')
-			;
-		while (n2[len2++] != '\0')
-			;
-		if (len1-- >= len2--)
-			len = len1;
+	while (n1[len1++] != '\0')
+		;
+	while (n2[len2++] != '\0')
+		;
+	if (len1-- >= len2--)
+		len = len1;
+	else
+		len = len2;
+	for (i = len; i >= 0; i--)
+	{
+		a1 = n1[len1 - 1] - '0';
+		a2 = n2[len2 - 1] - '0';
+		if (len1 >= 1)
+			len1--;
 		else
-			len = len2;
-		for (i = len; i >= 0; i--)
+			a1 = 0;
+		if (len2 >= 1)
+			len2--;
+		else
+			a2 = 0;
+		sum = overflow + a1 + a2;
+		if (sum > 9)
 		{
-			a1 = n1[len1 - 1] - '0';
-			a2 = n2[len2 - 1] - '0';
-			if (len1 >= 1)
-				len1--;
-			else
-				a1 = 0;
-			if (len2 >= 1)
-				len2--;
-			else
-				a2 = 0;
-			sum = overflow + a1 + a2;
-			if (sum > 9)
-			{
-				sum1 = sum % 10;
-				r[i] = sum1 + '0';
-				overflow = 1;
-			}
-			else
-			{
-				r[i] = sum + '0';
-				overflow = 0;
-			}
-		}
-		if (r[0] == '0')
-		{
-			if (len + 1 >= size_r && len >= size_r)
-				return (0);
-			for (j = 0; j <= len; j++)
-			{
-				r[j] = r[j + 1];
-			}
-			r[len] = '\0';
+			sum1 = sum % 10;
+			r[i] = sum1 + '0';
+			overflow = 1;
 		}
 		else
 		{
-			if (len + 1 >= size_r)
-				return (0);
-			r[len + 1] = '\0';
+			r[i] = sum + '0';
+			overflow = 0;
 		}
-		return (r);
 	}
+	if (r[0] == '0')
+	{
+		if (len + 1 >= size_r && len >= size_r)
+			return (0);
+		for (j = 0; j <= len; j++)
+		{
+			r[j] = r[j + 1];
+		}
+		r[len] = '\0';
+	}
+	else
+	{
+		if (len + 1 >= size_r)
+			return (0);
+		r[len + 1] = '\0';
+	}
+	return (r);
+}
