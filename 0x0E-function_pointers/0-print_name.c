516951dcf287869288ce1da83749eb7b18111242
sumin3 <suminyu@yahoo.com>
2018-07-25 17:53:12 +0000

    add new file

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - print a name
+ * @name: the name that need to print
+ * @f: pointer to function that prints a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+
+void print_name(char *name, void (*f)(char *));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /* _FUNCTION_H_ */
