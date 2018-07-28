a6faa6489430cfc95027afb61d112a9c3da0371c
sumin3 <suminyu@yahoo.com>
2018-07-28 16:39:06 +0000

    change the way i check if seperator is null, do nothing when seperator = null instead of return nothing

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -12,19 +12,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num_list;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
-	/* initialize num_list for n number of arguments */
-	va_start(num_list, n);
-	/* access all the arguments assigned to num_list */
-	for (i = 0; i < n; i++)
+	if (separator)
 	{
-		printf("%d", va_arg(num_list, int));
-		if (i < n - 1)
-			printf("%s", separator);
-	}
-	printf("\n");
+	/* initialize num_list for n number of arguments */
+		va_start(num_list, n);
+	/* access all the arguments assigned to num_list */
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(num_list, int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
 	/* clean memory reserved for num_list */
-	va_end(num_list);
+		va_end(num_list);
+	}
 
 }
