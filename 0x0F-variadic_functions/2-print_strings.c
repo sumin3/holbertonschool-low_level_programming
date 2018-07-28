51c8b784ff072e88b3614d6be6cdc9c99e04639a
sumin3 <suminyu@yahoo.com>
2018-07-28 17:22:49 +0000

    change logic - if separator is null, don't print the separator by using a tmp variable, and assign to empty string

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,9 +12,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list string_list;
 	unsigned int i;
 	char *tmp_string;
-
+	const char *tmp;
+	/* if sep is NULL, don't print it by changing it to empty string */
 	if (separator == NULL)
-		return;
+		tmp = "";
+	else /* otherwise, print the separator */
+		tmp = separator;
 	/* initialize string_list for n number of arguments */
 	va_start(string_list, n);
 	/* access all the arguments assigned to string_list */
@@ -25,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		printf("%s", tmp_string);
 		if (i < n - 1)
-			printf("%s", separator);
+			printf("%s", tmp);
 	}
 	printf("\n");
 	/* clean memory reserved for string_list */
