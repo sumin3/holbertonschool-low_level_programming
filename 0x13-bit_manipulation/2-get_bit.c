77a33a99a2bb48c139a75b8fb5bb17878c018463
sumin3 <suminyu@yahoo.com>
2018-08-09 20:31:37 +0000

    check if index over the range

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_idx;
 
-	if (n >> index == 0)
+	if (index >= sizeof(index) * 8)
 		return (-1);
 	bit_idx = (n >> index & 1) == 0 ? 0 : 1;
 	return (bit_idx);
