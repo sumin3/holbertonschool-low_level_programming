e595f1fb754b592e98270dc8a3bf0b9c29681710
sumin3 <suminyu@yahoo.com>
2018-08-14 19:20:46 +0000

    change everything

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,6 +1,24 @@
-# 0x14. C - File I/O
-filename | description
-0-read_textfile.c | reads a text file and prints it to the POSIX standard output.
-1-create_file.c | creates a file.
-2-append_text_to_file.c | appends a text at the end of a file.
-3-cp.c | copies the content of a file to another file.
+#include "holberton.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, num_char_write, num_char_read;
+	char *buffer = NULL;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	num_char_read = read(fd, buffer, letters);
+	close(fd);
+	num_char_write = write(1, buffer, num_char_read);
+	if (num_char_write < 0)
+		return (0);
+	else
+		return (num_char_write);
+	free(buffer);
+}
