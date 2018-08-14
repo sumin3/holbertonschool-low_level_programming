62f0eb5015521cb3c4e5379f3332ca6f9a3795b3
sumin3 <suminyu@yahoo.com>
2018-08-14 19:57:45 +0000

    add code for check if read function fail

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,10 +21,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	num_char_read = read(fd, buffer, letters);
+	if (num_char_read < 0)
+		return (0);
 	close(fd);
 	num_char_write = write(1, buffer, num_char_read);
 	free(buffer);
-	if (num_char_write < 0 || num_char_write != num_char_read)
+	if (num_char_write < 0 || (num_char_write != num_char_read))
 		return (0);
 	else
 		return (num_char_write);
