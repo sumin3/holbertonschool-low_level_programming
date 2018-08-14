1a7918863bb42d4904edaead3db9adf1a846dbc5
sumin3 <suminyu@yahoo.com>
2018-08-14 19:36:33 +0000

    move free function: free before return

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -23,9 +23,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	num_char_read = read(fd, buffer, letters);
 	close(fd);
 	num_char_write = write(1, buffer, num_char_read);
+	free(buffer);
 	if (num_char_write < 0)
 		return (0);
 	else
 		return (num_char_write);
-	free(buffer);
 }
