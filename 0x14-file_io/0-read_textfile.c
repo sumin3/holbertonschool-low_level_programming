51510da90c9f9e35c8c422c443f7292499997a24
sumin3 <suminyu@yahoo.com>
2018-08-14 20:09:53 +0000

    free when read function fail and move the  order for return 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -12,17 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, num_char_write, num_char_read;
 	char *buffer = NULL;
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 	num_char_read = read(fd, buffer, letters);
 	if (num_char_read < 0)
+	{
+		free(buffer);
 		return (0);
+	}
 	num_char_write = write(1, buffer, num_char_read);
 	free(buffer);
 	close(fd);
