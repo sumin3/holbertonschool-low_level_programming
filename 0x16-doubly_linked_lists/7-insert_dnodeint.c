a5c9962efdb41481e902842902fdbbb901c22675
sumin3 <suminyu@yahoo.com>
2018-09-15 21:32:36 +0000

    fix prev

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,11 +28,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = cur;
-	tmp = cur->prev->next;
+	tmp = cur;
 	cur->prev->next = new;
-	new = tmp;
-	cur->prev = new;
-
+	new = cur;
+	tmp->prev = new;
 	return (new);
 
 }
