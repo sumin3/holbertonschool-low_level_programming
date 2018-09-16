97d9b49335434648a91d629881808a22403edd69
sumin3 <suminyu@yahoo.com>
2018-09-16 08:24:20 +0000

    add new file

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,19 +7,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-        dlistint_t *cur = *head, *new = NULL, *tmp = NULL;
-        unsigned int i = 0;
+	dlistint_t *cur = *head, *tmp = NULL, *tmp1 = NULL;
+	unsigned int i = 0;
 
-        while (i < index && cur)
-        {
-                i++;
-                cur = cur->next;
-        }
-        if (i != index)
-                return (-1);
-	current->prev->next = cur->next;
-        tmp = cur->prev->next;
-        cur->prev->next = new;
-	return (new);
+	if ((*head) == NULL || head == NULL)
+		return (-1);
+	if (index == 0 && (*head) != NULL)
+	{
+		if ((*head)->next == NULL)
+		{
+			free(*head);
+			*head = NULL;
+			return (1);
+		}
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		(*head)->prev = NULL;
+		return (1);
+	}
+	for (i = 0; i < index && cur; i++)
+		cur = cur->next;
+	if (i == index)
+	{
+		if (cur->next == NULL)
+		{
+			cur->prev->next = NULL;
+			free(cur);
+			return (1);
+		}
+		tmp1 = cur->prev;
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+		tmp1->next = cur;
+		cur->prev = tmp1;
+		return (1);
+	}
+	else
+		return (-1);
 
 }
