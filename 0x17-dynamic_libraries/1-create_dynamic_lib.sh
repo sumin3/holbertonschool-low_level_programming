31530becfae2fd581abc34465a13640c7c29898f
sumin3 <suminyu@yahoo.com>
2018-09-17 19:43:28 +0000

    change sh filename

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -c -Wall -pedantic -Werror -Wextra -fPIC *.c
-gcc -Wall -pedantic -Werror -Wextra -shared -o libholberton.so *.o
+gcc -Wall -pedantic -Werror -Wextra -shared -o liball.so *.o
