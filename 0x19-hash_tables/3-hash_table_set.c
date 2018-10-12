9fae125f1f1d90c9ea3af8c242e47608528290a9
sumin3 <suminyu@yahoo.com>
2018-10-12 19:05:15 +0000

    return NULL if key is empty

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t  *tmp = NULL;
 
-	if (!ht || !key)
+	if (!ht || !key || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp = NULL;
 
-	if (ht == NULL || key == NULL)
+	if (!ht || !key || strlen(key) == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
