bdf92cb4752ac7e139160a3118cdbdcfb8b21874
sumin3 <suminyu@yahoo.com>
2018-11-16 23:13:28 +0000

    use diff method - increase result by 4 if the current cell is 1, then minus 1 if left, right, up, or down cell is 1

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -13,24 +13,13 @@ def island_perimeter(grid):
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
-                try:
-                    if i - 1 >= 0 and grid[i - 1][j] != 1:
-                        result += 1
-                except IndexError:
-                    result += 1
-                try:
-                    if grid[i + 1][j] != 1:
-                        result += 1
-                except IndexError:
-                    result += 1
-                try:
-                    if grid[i][j + 1] != 1:
-                        result += 1
-                except IndexError:
-                    result += 1
-                try:
-                    if j - 1 >= 0 and grid[i][j - 1] != 1:
-                        result += 1
-                except IndexError:
-                    result += 1
+                result += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                        result -= 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                        result -= 1
+                if j + 1 < len(grid[i]) and grid[i][j + 1] == 1:
+                        result -= 1
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                        result -= 1
     return (result)
