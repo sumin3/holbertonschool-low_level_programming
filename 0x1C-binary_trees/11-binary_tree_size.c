272c9633cc7d7c55051f2fef40d1ba251333e5eb
sumin3 <suminyu@yahoo.com>
2018-12-12 23:49:16 +0000

    check left and right is null or not

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -7,6 +7,11 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+		if (tree->left || tree->right)
+			return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	}
 	return (0);
 }
