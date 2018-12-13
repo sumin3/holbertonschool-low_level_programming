f2bb550645c3bbf4b6ea02d500750f58deb54e96
sumin3 <suminyu@yahoo.com>
2018-12-13 20:53:47 +0000

    change logic

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -10,24 +10,14 @@ int check_child(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree && (binary_tree_balance(tree) == 0) && check_child(tree))
-		return (1);
+	if (tree)
+	{
+		if (binary_tree_balance(tree) == 0)
+			return (1);
+		binary_tree_is_perfect(tree->left);
+		binary_tree_is_perfect(tree->right);
+	}
 	return (0);
-}
-
-/**
- * check_child - check if the node of the tree has two chils or not
- * @tree: a pointer to the root node of the tree to measure the balance factor
- * Return: return 1 if has two childs, otherwise, return 0.
- * If tree is NULL, return 0
- */
-int check_child(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	return (check_child(tree->left) && check_child(tree->right));
 }
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
