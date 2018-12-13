963ac0e2f1d9c3b74e4c4d0607176a98b9e3be4b
sumin3 <suminyu@yahoo.com>
2018-12-13 21:14:07 +0000

    change logic

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -10,16 +10,17 @@ int check_child(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree)
+	if (!tree || binary_tree_balance(tree) != 0)
+		return (0);
+	if (tree->left && (binary_tree_balance(tree) == 0))
 	{
-		if (binary_tree_balance(tree) == 0)
-		{
-			binary_tree_is_perfect(tree->left);
-			binary_tree_is_perfect(tree->right);
-			return (1);
-		}
+		return (binary_tree_is_perfect(tree->left));
 	}
-	return (0);
+	if (tree->right && (binary_tree_balance(tree) == 0))
+	{
+		return (binary_tree_is_perfect(tree->right));
+	}
+	return (1);
 }
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
